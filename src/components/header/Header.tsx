b456f5d5b024a21f89ec7f87bf01ad68caba60a1
Michael Krilov <MishaBobyM@gmail.com>
2022-05-05 20:56:37 +0500

    fix: resolve conflicts

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,39 @@
+import PersonIcon from '@mui/icons-material/Person';
+import LoginIcon from '@mui/icons-material/Login';
+
+import './header.scss';
+
 const Header = () => {
-  
-}
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="header__left">
+            <div className="header__logo">
+              <h1>Trello</h1>
+            </div>
+
+            <nav className="header__nav">
+              <div className="header__nav-link">Home</div>
+              <div className="header__nav-link">Boards</div>
+              <div className="header__nav-link">Profile</div>
+            </nav>
+          </div>
+
+          <div className="header__user">
+            <button className="header__user-entry">
+              <LoginIcon />
+              Login
+            </button>
+            <button className="header__user-registr">
+              <PersonIcon />
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
