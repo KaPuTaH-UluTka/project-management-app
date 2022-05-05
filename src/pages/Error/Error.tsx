6ccd6545795126c3f5e5f7a68d0cadccad63d836
unknown <anterebol@gmail.com>
2022-05-05 23:03:30 +0300

    feat: add Error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,3 +1,23 @@
+import robot from '../../assets/robot.gif';
+import { NavLink } from 'react-router-dom';
+import { pathes } from '../../pathes/pathes';
+import Button from '@mui/material/Button';
+
+import './error.scss';
+
 export const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <div className="error__block">
+        <img src={robot} alt="Error 404" />
+        <p>Ooops...</p>
+        <p>Anything want wrong...</p>
+        <NavLink to={pathes.main}>
+          <Button variant="contained" color="warning">
+            Return
+          </Button>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
