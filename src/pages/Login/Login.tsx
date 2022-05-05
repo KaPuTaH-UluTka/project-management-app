b758766e066e891f8cf216caad8378940efa2952
Alexei <lexas427@gmail.com>
2022-05-05 23:07:53 +0300

    feat: implement basick layout for login page and create root styles

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,134 @@
 import { useAppSelector } from '../../hooks/hooks';
-import { Navigate } from 'react-router-dom';
-import { pathes } from '../../pathes/pathes';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import { useState } from 'react';
+import { rootStyles, violetTheme } from '../../style/rootStyles';
+
 export const Login = () => {
   const { isLogined } = useAppSelector((state) => state.loginReducer);
-  return isLogined ? <Navigate to={pathes.main} /> : <div>Login</div>;
+  const [signUp, setSignUp] = useState(false);
+
+  function changeForm() {
+    setSignUp(!signUp);
+  }
+
+  return (
+    <ThemeProvider theme={violetTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1 }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            {signUp && (
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoFocus
+              />
+            )}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              color="primary"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            {signUp ? (
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  background: rootStyles.violetLight,
+                  ':hover': {
+                    background: rootStyles.violetDark,
+                  },
+                }}
+              >
+                Sign Up
+              </Button>
+            ) : (
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  background: rootStyles.violetLight,
+                  ':hover': {
+                    background: rootStyles.violetDark,
+                  },
+                }}
+              >
+                Sign In
+              </Button>
+            )}
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={changeForm}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
