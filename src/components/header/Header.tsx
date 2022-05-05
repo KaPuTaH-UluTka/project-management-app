69b48f092ee6aed3563bf53168b3316da2c1a633
unknown <anterebol@gmail.com>
2022-05-05 21:59:24 +0300

    feat: add links in header and some functional for login/logout user

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,14 @@
 import PersonIcon from '@mui/icons-material/Person';
 import LoginIcon from '@mui/icons-material/Login';
-
+import { pathes } from '../../pathes/pathes';
+import { Link } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
+import { addLogin } from '../../store/Reducer/loginReducer/loginReducer';
 import './header.scss';
 
 const Header = () => {
+  const { isLogined } = useAppSelector((state) => state.loginReducer);
+  const dispatch = useAppDispatch();
   return (
     <header className="header">
       <div className="container">
@@ -13,23 +18,42 @@ const Header = () => {
               <h1>Trello</h1>
             </div>
 
-            <nav className="header__nav">
-              <div className="header__nav-link">Home</div>
-              <div className="header__nav-link">Boards</div>
-              <div className="header__nav-link">Profile</div>
-            </nav>
+            {isLogined ? (
+              <nav className="header__nav">
+                <Link to={pathes.main}>
+                  <div className="header__nav-link">Home</div>
+                </Link>
+                <Link to={pathes.board}>
+                  <div className="header__nav-link">Add board</div>
+                </Link>
+                <Link to={pathes.edit}>
+                  <div className="header__nav-link">Edit Profile</div>
+                </Link>
+              </nav>
+            ) : null}
           </div>
 
-          <div className="header__user">
-            <button className="header__user-entry">
+          {!isLogined ? (
+            <div className="header__user">
+              <Link to={pathes.login}>
+                <button className="header__user-entry">
+                  <LoginIcon />
+                  Login
+                </button>
+              </Link>
+              <Link to={pathes.login}>
+                <button className="header__user-registr">
+                  <PersonIcon />
+                  Sign up
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <button className="header__user-entry output" onClick={() => dispatch(addLogin())}>
               <LoginIcon />
-              Login
+              Logout
             </button>
-            <button className="header__user-registr">
-              <PersonIcon />
-              Sign up
-            </button>
-          </div>
+          )}
         </div>
       </div>
     </header>
