bfc5f5a651e6a56a205b2cd31299cbd0ce0e991d
Alexei <lexas427@gmail.com>
2022-05-28 20:00:53 +0300

    refactor: del file upload logic

--- src/components/updateTaskModal/UpdateTaskModal.tsx
+++ src/components/updateTaskModal/UpdateTaskModal.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Button from '@mui/material/Button';
 import { closeModal } from '../../store/Reducer/confirmationReducer/confirmationReducer';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { useFormik } from 'formik';
-import {
-  Box,
-  Container,
-  ImageList,
-  ImageListItem,
-  Input,
-  TextField,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
-import { downloadFile, updateTaskViaModal, uploadFile } from '../../store/api/taskApi';
+import { Box, Container, Input, TextField, ThemeProvider, Typography } from '@mui/material';
+import { updateTaskViaModal } from '../../store/api/taskApi';
 import * as yup from 'yup';
 import { useParams } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
@@ -29,9 +20,7 @@ import {
   titleInputStyle,
   titleStyle,
 } from './updateTaskModalStyles';
-import { rootStyles, violetTheme } from '../../style/rootStyles';
-import { ITaskFilesInfo } from '../../types/types';
-import UploadBtn from './uploadBtn/UploadBtn';
+import { violetTheme } from '../../style/rootStyles';
 
 const UpdateTaskModal = () => {
   const dispatch = useAppDispatch();
@@ -39,16 +28,9 @@ const UpdateTaskModal = () => {
   const { order, taskId, columnId, done, userId } = useAppSelector(
     (state) => state.openModalReducer
   );
-  const { taskTitle, taskDesc, taskFilesInfo, taskFiles } = useAppSelector(
-    (state) => state.apiReducer
-  );
+  const { taskTitle, taskDesc } = useAppSelector((state) => state.apiReducer);
   const [titleState, setTitleState] = useState(false);
   const [descState, setDescState] = useState(false);
-  useEffect(() => {
-    taskFilesInfo.forEach((e: ITaskFilesInfo) =>
-      dispatch(downloadFile({ taskId: taskId, filename: e.filename }))
-    );
-  }, []);
   const updateTaskSchema = yup.object({
     title: yup.string().required(),
     description: yup.string().required(),
@@ -95,15 +77,6 @@ const UpdateTaskModal = () => {
   function cancelChangeDesc() {
     updateTaskForm.values.description = updateTaskForm.values.prevDescription;
     changeDesc();
-  }
-  async function handleUpload(e: React.FormEvent) {
-    const target = e.target as HTMLInputElement;
-    if (target.files) {
-      const formData = new FormData();
-      formData.append('taskId', taskId);
-      formData.append('file', target.files[0]);
-      dispatch(uploadFile(formData));
-    }
   }
 
   return (
@@ -163,25 +136,6 @@ const UpdateTaskModal = () => {
                 {updateTaskForm.values.description}
               </Typography>
             )}
-          </Container>
-          <Container>
-            <Typography sx={{ pr: 1, fontSize: 18 }}>
-              <FormattedMessage id="updateModal.attachment" defaultMessage="Attachments:" />
-            </Typography>
-            <ImageList
-              sx={{ height: 298, backgroundColor: rootStyles.gray, padding: 1 }}
-              cols={1}
-              gap={5}
-            >
-              {taskFiles.map((e, i) => {
-                return (
-                  <ImageListItem key={i}>
-                    <img src={`${e}`} alt={'taskImage'} loading="lazy" />
-                  </ImageListItem>
-                );
-              })}
-            </ImageList>
-            <UploadBtn handleUpload={handleUpload} />
           </Container>
           <Container sx={btnContainerStyle}>
             <Button variant="contained" color="primary" sx={{ marginRight: 1 }} type="submit">
